fc9753a88af27d3027c520f9d9da5827507b123d
dhruvsinghk <dhruvk1989>
2024-06-23 21:59:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { Hero, Footer, PopularProducts, SpecialOffers, Subscribe, SuperQuality, CustomerReviews, Services} from './sections'
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='relative'>
+      <Nav/>
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts/>
+      </section>
+      <section className='padding'>
+        <SuperQuality/>
+      </section>
+      <section className='padding-x py-10'>
+        <Services/>
+      </section>
+      <section className='padding'>
+        <SpecialOffers/>
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews/>
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe/>
+      </section>
+      <section className='padding bg-black padding-x padding-t pb-8'>
+        <Footer/>
+      </section>
+    </main>
+  )
 }
 
 export default App;
