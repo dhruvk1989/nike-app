4b32162b5b9051b7e4e29b3aa1e185650050c138
dhruvsinghk <dhruvk1989>
2024-06-23 22:01:13 +0530

    second commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+
+export {
+    Hero,
+    Footer,
+    PopularProducts,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Services
+}
